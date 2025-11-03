061c055224a6607b63de6b267240df5ed03fda51
Bouncybull <vinceschoutrop@gmail.com>
2025-11-03 11:48:15 +0100

    Fixed Scene loader and Made first blockout of level 2

--- Ruin Jumper/Assets/Scripts/SceneSwitch.cs
+++ Ruin Jumper/Assets/Scripts/SceneSwitch.cs
@@ -4,24 +4,18 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     private Transform ColliderTransform;
-    public string next_Scene;
+    public int next_Scene;
     public int exit_ID;
-    private PlayerMovement2D playerMovement;
 
-    void Start()
+
+
+    private void OnTriggerEnter(Collider collision)
     {
-        playerMovement = GetComponent<PlayerMovement2D>();
-        Collider collider = ColliderTransform.GetChild(0).GetComponent<Collider>();
+        if(collision.tag == "Player")
+        {
+            print("Beep");
+            StaticData.exit_Id = exit_ID;
+            SceneManager.LoadScene(next_Scene);   
+        }
     }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        print("Beep");
-        StaticData.exit_Id = exit_ID;
-        playerMovement.SaveVelocity();
-        SceneManager.LoadScene(next_Scene);
-    }
-
-
 }
